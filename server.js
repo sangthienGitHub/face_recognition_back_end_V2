3e8730dab800e6eb3af3e81ef8cf068d3cb63f47
sangTran <sangthien2010@gmail.com>
2020-05-01 12:48:49 -0500

    add docker compose file and morgan log file

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const bcrypt = require("bcrypt-nodejs");
 const cors = require("cors");
 const knex = require("knex");
 require("dotenv").config();
+const morgan = require("morgan");
 
 const register = require("./controllers/register");
 const signin = require("./controllers/signin");
@@ -22,11 +23,12 @@ const db = knex({
 
 const app = express();
 
+app.use(morgan("combined"));
 app.use(cors());
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.send(db.users);
+  res.send("Its working");
 });
 app.post("/signin", signin.handleSignin(db, bcrypt));
 app.post("/register", (req, res) => {
